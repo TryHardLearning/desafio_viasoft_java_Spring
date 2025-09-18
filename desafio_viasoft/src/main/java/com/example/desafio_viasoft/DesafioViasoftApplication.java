bf23705f2328a5b89def2fdb486feb8241bb75a3
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-09-18 20:42:32 -0300

    start

---
+++ desafio_viasoft/src/main/java/com/example/desafio_viasoft/DesafioViasoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_viasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioViasoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioViasoftApplication.class, args);
+    }
+
+}
