11ca0b1ba9511c64fa7006fbc09ca7e63c396181
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-09-18 20:44:03 -0300

    start

---
+++ desafio_viasoft/src/test/java/com/example/desafio_viasoft/DesafioViasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_viasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioViasoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
